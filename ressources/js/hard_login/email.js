4d6cfe6bc54ae1f48af1292e897f8c0ddf8a9fd4
benhalimam <matteo.benhalima@etu.umontpellier.fr>
2025-12-04 21:27:52 +0100

    feat: rajout champ email

---
+++ ressources/js/hard_login/email.js
@@ -0,0 +1,23 @@
+let email_domain_textfield = document.getElementById('email_domain_text')
+document.getElementById('email_domain').addEventListener('change', function () {
+  if (this.value === "custom") {
+    email_domain_textfield.style.display = "inline"
+  }else{
+    email_domain_textfield.style.display = "none"
+  }
+});
+
+function addCustomRandomPos(){
+    list = document.getElementById("email_domain")
+
+    const option = document.createElement("option");
+    option.textContent = "personnalisé";
+    option.value = "custom";
+
+    index = list.options.length * 0.4 + Math.random() * list.options.length * 0.55
+    index = Math.floor(index)
+    list.insertBefore(option, list.options[index]);
+    
+}
+
+addCustomRandomPos()
