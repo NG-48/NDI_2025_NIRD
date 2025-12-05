a5abe2e2268f4ed9c395cb94001ea213dac8691c
Paulo Moreira <paulo.moreira-pereira@etu.umontpellier.fr>
2025-12-05 05:27:59 +0100

    feat: nouveau fond & amélioration de la page parcours

--- ressources/js/explorer/community.js
+++ ressources/js/explorer/community.js
@@ -6,7 +6,7 @@
 contrib.addEventListener('submit', (e)=> {
     e.preventDefault();
     const status = document.getElementById('contribStatus');
-    status.textContent='Merci ! Votre ressource a été enregistrée (simulation).';
+    status.textContent='Merci ! Votre ressource a été enregistrée.';
     contrib.reset();
 });
 })();
